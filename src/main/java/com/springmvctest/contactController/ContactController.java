ec1d0c9d48c3376ae17a553e78c172ddd4372841
bulatovav <bulatovav@vdcom.ru>
2019-02-20 13:59:22 +0400

    Update pom structure and chenged version dependencies. Doesn't work first page.

--- src/main/java/com/springmvctest/contactController/ContactController.java
+++ src/main/java/com/springmvctest/contactController/ContactController.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -24,7 +24,7 @@ public class ContactController {
         this.contactService = contactService;
     }
 
-    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
     public String list(Model model) {
         LOG.info("Logger contacts");
         List<Contact> contacts = contactService.findAll();
--- src/main/java/com/springmvctest/contactService/ContactServiceImpl.java
+++ src/main/java/com/springmvctest/contactService/ContactServiceImpl.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import com.springmvctest.repositories.ContactRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
