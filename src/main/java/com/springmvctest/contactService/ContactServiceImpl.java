0d5742725f4d3655e2ad061d43ed6fdd9f5784ac
bulatovav <bulatovav@vdcom.ru>
2019-02-20 16:01:58 +0400

    Update pom structure. Doesn't work first page.

--- src/main/java/com/springmvctest/contactService/ContactServiceImpl.java
+++ src/main/java/com/springmvctest/contactService/ContactServiceImpl.java
@@ -3,14 +3,11 @@ package com.springmvctest.contactService;
 import com.google.common.collect.Lists;
 import com.springmvctest.contacts.Contact;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import com.springmvctest.repositories.ContactRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -37,12 +34,5 @@ public class ContactServiceImpl implements ContactService {
     @Autowired
     public void setContactRepository(ContactRepository contactRepository) {
         this.contactRepository = contactRepository;
-    }
-
-    private Contact mapRowToContact(ResultSet resultSet, int rowNum) throws SQLException {
-        return new Contact(
-                resultSet.getString("first_name"),
-                resultSet.getString("last_name")
-        );
     }
 }
